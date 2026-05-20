eec9146ac1e1c0f2a3aabd66bac5c4c0c083dab6
= <VMGMaster0@gmail.com>
2026-05-20 12:44:54 -0300

    estágio inicial do sistema de nitificação

--- js-interface/front.js
+++ js-interface/front.js
@@ -27,13 +27,25 @@ menu.addEventListener('click', abrirMenu);
 
 function Adicionar_ao_carrinho(id,nome,preco){
   const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-    let item = carrinho.find(item=> item.id === id)
-    
-    if(item){
-        item.quantidade+=1;
-    }else{
-        carrinho.push({'id':id, 'nome':nome, 'preco':preco, 'quantidade':1});
-    }
 
-    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+  let item = carrinho.find(item=> item.id === id)
+  
+  if(item){
+    item.quantidade+=1;
+  }else{
+    carrinho.push({'id':id, 'nome':nome, 'preco':preco, 'quantidade':1});
+  }
+
+  localStorage.setItem('carrinho', JSON.stringify(carrinho));
+
+  Notificar_adicao_carrinho();
+    
+}
+function Notificar_adicao_carrinho() {
+
+  console.log("notificando ...")
+
+  let notificacao = document.getElementById("notificacao");
+  notificacao.className = "show";
+  setTimeout(function(){ notificacao.className = notificacao.className.replace("show", ""); }, 3000);
 }
